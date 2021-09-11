725f6fa3c3b25a2476fd40d126604c53a0026170
atif shafi <atifshafi63@gmail.com>
2021-09-11 20:18:08 +0530

    finishing touches

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,5 @@
 from asyncio.tasks import create_task, sleep
+from django.contrib.messages.api import add_message
 from django.http import response
 from django.shortcuts import redirect, render
 import asyncio
@@ -9,6 +10,8 @@ import datetime as dt
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from asgiref.sync import async_to_sync, sync_to_async
+import pickle
+from django.contrib import messages
 # Create your views here.
 
 
@@ -39,23 +42,28 @@ async def getcurrent_weather(city_name,country_code,key):
     url = 'http://'+'api.openweathermap.org/data/2.5/weather?q'
     async with aiohttp.ClientSession() as session:
         async with session.get(f'{url}={city_name},{state_code},{country_code}&appid={key}') as response:
-            # if response.status == '200':
-            data = await response.json()
-            print(response.status)
-            # await asyncio.sleep(4)
-            print('executed after 4 seconds')
-            return data
+            if response.status != 404:
+                data = await response.json()
+                return data
+            else:
+                return None
+
+
+
+# def serialize_data(obj,request=''):
+#     # print(obj['forecast'])
+#     for data in obj['forecast']:
+#         if data == 'main':
+#             data['main']['date'] = str(data['main']['date'])
+    
+#     print(type(obj['forecast'][0]['main']['date']))
 
 
 
 
+#view for fetching weather
 
-
-
-
-#view for fetchign weather
-
-async def fetch_weather(request):
+def fetch_weather(request):
     if request.method == 'GET':
         city_name = request.GET.get('city')
         key = '48cae9af5bbad31b93b51b6db42dea12'
@@ -63,75 +71,84 @@ async def fetch_weather(request):
         if 'country_code' in request.GET:
             country_code = request.GET.get('country_code')
         else:
-            country_code = ''
+            country_code = None
         
-        current_weather, forecast_weather = await asyncio.gather(getcurrent_weather(city_name,country_code,key),get_forecastweather(city_name,key))
-        print('Forecast',forecast_weather.keys())
-       
-        print('response came after 4 seconds')
-    
+        current_weather  = asyncio.run(getcurrent_weather(city_name,country_code,key))
+        forecast_weather  = asyncio.run(get_forecastweather(city_name,key))
+        print('response ',current_weather)
         keys = current_weather
-        data ={}
-        data['city_name']=keys['name']
-        data['description']=keys['weather'][0]['description']
-        data['temp'] = round(kelvin_to_celisus(keys['main']['temp']),2) 
-        data['feels_like'] = round(kelvin_to_celisus(keys['main']['feels_like']),2)
-        data['max_temp'] = round(kelvin_to_celisus(keys['main']['temp_max']),2)
-        data['min_temp'] = round(kelvin_to_celisus(keys['main']['temp_min']),2)
-        data['sunrise'] = ConverTime(UtcToIst(keys['sys']['sunrise'])) 
-        data['sunset'] = ConverTime(UtcToIst(keys['sys']['sunset']))
+        if keys!= None:
+            data ={}
+            data['date'] = keys['dt']
+            data['city_name']=keys['name']
+            data['description']=keys['weather'][0]['description']
+            data['temp'] = round(kelvin_to_celisus(keys['main']['temp']),2) 
+            data['feels_like'] = round(kelvin_to_celisus(keys['main']['feels_like']),2)
+            data['max_temp'] = round(kelvin_to_celisus(keys['main']['temp_max']),2)
+            data['min_temp'] = round(kelvin_to_celisus(keys['main']['temp_min']),2)
+            data['sunrise'] = ConverTime(UtcToIst(keys['sys']['sunrise'])) 
+            data['sunset'] = ConverTime(UtcToIst(keys['sys']['sunset']))
 
-        forecast = {}
-        
+        # print(data)
 
-        for keys,values in forecast_weather.items():
-            if keys == 'list':
-                forecast['forecast'] = values
-            if keys == 'city':
-                forecast['city'] = values
+            forecast = {}
+            for keys,values in forecast_weather.items():
+                if keys == 'list':
+                    forecast['forecast'] = values
+                if keys == 'city':
+                    forecast['city'] = values
 
-        updated_forcast = {}
-        updated_forcast['forecast'] = []
-        date_changed = False
 
-        # modifying forecast data removing redundancies 
-        prev = None
-        for items in forecast['forecast']:
 
-            for key,value in items.items():
-                if key == 'dt_txt':
-                    items[key] = dt.datetime.strptime(value,'%Y-%m-%d %H:%M:%S')
-                    date_ = items[key].date()
-                    if prev != date_:
-                        date_changed = True
-                        
-                    prev = date_
-            print(date_changed)
-            if date_changed:    
-                updated_forcast['forecast'].append(items)
-                date_changed = False
+ # modifying forecast data removing redundancies 
+            updated_forcast = {}
+            updated_forcast['forecast'] = []
+            date_changed = False
+
+            prev = None
+            for items in forecast['forecast']:
+
+                for key,value in items.items():
+                    #modifying date value and comparing it as well
+                    if key == 'dt_txt':
+                        items[key] = dt.datetime.strptime(value,'%Y-%m-%d %H:%M:%S')
+                        date_ = items[key].date()
+                        if prev != date_:
+                            date_changed = True
+                            
+                        prev = date_
+                if date_changed:    
+                    updated_forcast['forecast'].append(items)
+                    date_changed = False
 
                    
      
 
-        for items in updated_forcast['forecast']:
-         
-            for key,value in items.items():
-                if key == 'dt_txt':
-                    items['main']['date'] = items[key].date()
-                    items['main']['day'] = items[key].strftime("%A")
-                    items['main']['temp'] = kelvin_to_celisus(items['main']['temp'])
-                    items['main']['temp_min'] = kelvin_to_celisus(items['main']['temp_min'])
-                    items['main']['feels_like'] = kelvin_to_celisus(items['main']['feels_like'])
-                    items['main']['temp_max'] = kelvin_to_celisus(items['main']['temp_max'])
+            for items in updated_forcast['forecast']:
+            
+                for key,value in items.items():
+                    if key == 'dt_txt':
+                        items['dt'] = UtcToIst(items['dt']).split(' ')[0]
+                        items['main']['date'] = items[key].date()
+                        items['main']['day'] = items[key].strftime("%A")
+                        items['main']['temp'] = kelvin_to_celisus(items['main']['temp'])
+                        items['main']['temp_min'] = kelvin_to_celisus(items['main']['temp_min'])
+                        items['main']['feels_like'] = kelvin_to_celisus(items['main']['feels_like'])
+                        items['main']['temp_max'] = kelvin_to_celisus(items['main']['temp_max'])
 
-        updated_forcast['city'] = forecast['city']
-        updated_forcast['city']['sunrise'] = ConverTime(UtcToIst(updated_forcast['city']['sunrise']))     
-        updated_forcast['city']['sunset'] = ConverTime(UtcToIst(updated_forcast['city']['sunset']))   
-        print(updated_forcast['city'])
-
-        days_range = range(1,5)
-        print('Range ',days_range)
+            updated_forcast['city'] = forecast['city']
+            updated_forcast['city']['sunrise'] = ConverTime(UtcToIst(updated_forcast['city']['sunrise']))     
+            updated_forcast['city']['sunset'] = ConverTime(UtcToIst(updated_forcast['city']['sunset']))   
+            
+            session_data = json.dumps(updated_forcast.copy(),indent=4, sort_keys=True, default=str)
+            request.session['data'] = session_data
+            
+            data['date'] = UtcToIst(data['date']).split(' ')[0]
+        else:
+            data = None
+            updated_forcast = None
+            messages,add_message(request,messages.INFO,'Unable to find the city')
+            
         context ={
             'data': data,
             'forecast':updated_forcast
@@ -150,23 +167,41 @@ async def get_location_iq(key,longi,lati):
             return data
 
 
+#getting position coordinates
 import json
 import concurrent.futures 
 @csrf_exempt
 def get_location(request):
-    print('working')
     if request.method == 'POST':
         key = 'pk.736fba620749123a25d7355ff16b2e52'
         get_data = json.loads(request.body.decode('utf-8'))
-
-        coroutine = get_location_iq(key,get_data['longitude'],get_data['latitude'])
-        result = asyncio.run(coroutine)       
+        result = asyncio.run(get_location_iq(key,get_data['longitude'],get_data['latitude']))       
         data = result
         return JsonResponse(data,safe=False)
+        
 
 
 
 
 
 def weather_detail(request):
-    return render(request,'')
+    if request.method == 'GET':
+        forecast_data = json.loads(request.session['data'])
+        filter_date = request.GET.get('date')
+        data_context = None
+        if filter_date:
+            print(filter_date)
+            for data in forecast_data['forecast']:
+                print(data['dt'])
+                if data['dt'] == filter_date:
+                    data_context = data
+                    break
+        print('Matched data ',data_context)
+        data ={}
+        data['city'] = forecast_data['city']
+        data['forecast'] = data_context
+
+        print(data)
+
+        # print('Result from thread ',result)
+        return render(request,'weather/weather_detail.html',data)
